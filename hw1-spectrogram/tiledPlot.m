e5bdc96beee0e98ea7abbab83e6bfc603083aaa3
md-sid <siddat.md.123@gmail.com>
2022-09-19 20:27:53 -0600

    tiled layout with zoom feature

--- hw1-spectrogram/tiledPlot.m
+++ hw1-spectrogram/tiledPlot.m
@@ -1,23 +1,32 @@
-function tiledPlot(x, sMag, t, time, f, modeTitle)
-%tiledPlot(x, sMag, t, time, f, modeTitle) is a helper function for
-%customSpectrogram
+function tiledPlot(x, sMag, fs, t, time, f, modeTitle, zoomFlag)
+%tiledPlot(x, sMag, fs, t, time, f, modeTitle, zoomFlag) is a helper
+%function for customSpectrogram
 % 
 
 figure;
 tiledlayout(3, 4);
 
-nexttile([2 1]);
-cumFreq = sum(sMag, 2);
-plot(cumFreq, 'LineWidth', 1.5); view([-90 90]);
+P2 = abs(fft(x) / length(x));
+P1 = P2(1 : length(x)/2 + 1);
+P1(2 : end - 1) = 2 * P1(2 : end - 1);
+ff = fs * (0 : (length(x) / 2)) / length(x);
+
+nexttile(1, [2 1]);
+if zoomFlag == 1
+    tmp = find(ff >= f(1));
+    fSamples = tmp(1);
+    tmp = find(ff <= f(end));
+    fSamples = fSamples : tmp(end);
+    ff = ff(fSamples);
+    P1 = P1(fSamples);
+end
+plot(ff, P1); view([-90 90]);
 title('Spectrum');
 
-nexttile([2 3]);
+nexttile(2, [2 3]);
 plotSpectrogram(t, f, sMag, modeTitle);
 
-nexttile();
-axis off;
-
-nexttile([1 3]);
+nexttile(10, [1 3]);
 plot(time, x);
 xlabel('Time (s)'); title('Time Domain Waveform');
 
