9541432e59269f9f109a0c2093caf6c63b31cea4
md-sid <siddat.md.123@gmail.com>
2022-09-19 20:33:35 -0600

    recorded another audio

--- hw1-spectrogram/record_audio.m
+++ hw1-spectrogram/record_audio.m
@@ -3,11 +3,12 @@ clear; clc; close all;
 Fs = 8000;  % sampling frequency in Hertz
 nBits = 16; % number of bits
 nChan = 1;  % 1 for mono, 2 for stereo
-recDur = 5; % in seconds
+recDur = 4; % in seconds
 
 recObj = audiorecorder(Fs, nBits, nChan);
-disp("Read the following:\n");
-disp("Are your grades higher or lower than Nancy's? (sx10)");
+disp("Read the following:");
+% disp("Are your grades higher or lower than Nancy's? (sx10)");
+disp("Last year's gas shortage caused steep price increases. (sx67)");
 recordblocking(recObj, recDur);
 disp("End of Recording!");
 
@@ -16,4 +17,5 @@ play(recObj);   % play the recording
 y = getaudiodata(recObj);   % stores data in double
 plot(y);
 
-audiowrite('sx10.wav', y, Fs);  % save the audio in same directory
+% audiowrite('sx10.wav', y, Fs);  % save the audio in same directory
+audiowrite('sx67.wav', y, Fs);
