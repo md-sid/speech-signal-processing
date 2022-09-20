50891411411a98e6d07e5b1df370d8917a27d8a8
md-sid <siddat.md.123@gmail.com>
2022-09-19 23:55:26 -0600

    modified for wide band

--- hw1-spectrogram/main.m
+++ hw1-spectrogram/main.m
@@ -9,8 +9,8 @@ params.Fs = fs;
 params.nFFT = 1024;
 % need to specify window type
 % 0 for hanning, 1 for hamming, 2 for rectwin, 3 for kaiser, 4 for chebwin
-params.window = [1, 200];
-params.nOverlap = 50;
+params.window = [1, 10];
+params.nOverlap = 2;
 params.fsst = 0;    % use 1 to use FSST
 params.plotType = 1;    % if 0 or not used then plots only spectrogram
 params.zoom = 1;    % 1 to zoom into spectrogram with given boundaries
