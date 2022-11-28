2ff2c13815d436f65d8ea3a1cc1b5efc6618afef
Siddat <57923667+md-sid@users.noreply.github.com>
2022-11-28 16:03:37 -0700

    fixed bugs

--- hw4-em-algorithm/em_alogithm.m
+++ hw4-em-algorithm/em_alogithm.m
@@ -14,24 +14,31 @@ z_im = ones(N, nClusters);
 for iter = 1 : nIter
     fprintf('Iteration %d of %d\n', iter, nIter);
     % E step
-    p_xi_zim =  zeros(1, nClusters);
+    
     for i = 1 : N
+        p_xi_zim =  zeros(1, nClusters);
         for m = 1 : nClusters
             tmp = data(i, :) - mu_m(m, :);
             p_xi_zim(m) = (exp(-0.5 * tmp / ...
-                (sigma_m(:, :, m)) * tmp')) / ...
-                ((2 * pi) ^ (d / 2) * sqrt(det(sigma_m(:, :, m))));
-            z_im(i,m) = (p_xi_zim(m) * pi_m(m)) / (sum(p_xi_zim .* pi_m));
-        end 
+                (sigma_m(:, :, m)) * tmp.')) / ...
+                (((2 * pi) ^ (d / 2)) * sqrt(det(sigma_m(:, :, m))));
+            z_im(i, m) = (p_xi_zim(m) * pi_m(m)) / (sum(p_xi_zim .* pi_m));
+        end
+%          z_im(i, :) = z_im(i, :) / (sum(p_xi_zim .* pi_m));
     end
 
     % M step
     for m = 1 : nClusters
-        tmp = data - mu_m(m);
-        sigma_m(:, :, m) = z_im(:, m)' .* tmp' * tmp / sum(z_im(:, m));
-        mu_m(m, :) = (z_im(:, m)' * data) / sum(z_im(:, m));
+        tmp = data - mu_m(m, :);
+        sigma_m(:, :, m) = z_im(:, m).' .* tmp.' * tmp / sum(z_im(:, m));
+        mu_m(m, :) = (z_im(:, m).' * data) / sum(z_im(:, m));
         pi_m(m) = sum(z_im(:, m)) / N;
     end
+    scatter(data(:, 1), data(:, 2), '.');
+    hold on;
+    scatter(mu_m(:, 1), mu_m(:, 2), 'red', 'filled');
+    hold off;
+    pause(1e-5);
 end
 
 figure;
@@ -45,5 +52,4 @@ end
 hold off;
 
 saveas(gcf, 'em_with_error_ellipse.png');
-
 
--- hw4-em-algorithm/error_ellipse.m
+++ hw4-em-algorithm/error_ellipse.m
@@ -8,12 +8,12 @@ function error_ellipse(cov_mat, mu)
 % chi squared quantile
 k = 2.4477; % skipped the calculation here. For details see the url
 
-n=100; % Number of points around ellipse
-p=0:pi/n:2*pi; % angles around a circle
+n = 100; % Number of points around ellipse
+p = 0 : pi / n : 2 * pi; % angles around a circle
 [eigVec, eigVal] = eig(cov_mat);    % compute eigen vector and eigen values
-xy = [cos(p'),sin(p')] * sqrt(eigVal) * eigVec'; % Transformation
-x = xy(:,1);
-y = xy(:,2);
+xy = [cos(p'), sin(p')] * sqrt(eigVal) * eigVec'; % Transformation
+x = xy(:, 1);
+y = xy(:, 2);
 plot((mu(1) + k * x), (mu(2) + k * y), LineWidth = 1.5);
 end
 
